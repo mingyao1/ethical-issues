37e5a820c28ea72422c41a16dcec5ec109b37e50
mingyao1 <106688909+mingyao1@users.noreply.github.com>
2023-09-07 14:27:53 -0500

    basic style

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,31 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 import Question from "./Question"
 
 
 const Home = () => {
+    const location = useLocation();
 
     return <>
 
         <div className='container'>
-            <header className="mt-2">
-                <h1>Group 1</h1>
+            <header className="mt-4 mb-4 text-center">
+                <h1 className="display-4">Group 1</h1>
+                <nav className="d-flex justify-content-end">
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}>Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/about" className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}>About Us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/how-to-use" className={`nav-link ${location.pathname === '/how-to-use' ? 'active' : ''}`}>How to Use</Link>
+                        </li>
+                    </ul>
+                </nav>
             </header>
-            <nav>
-                <ul className="nav">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">About Us</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/how-to-use" className="nav-link">How to Use</Link>
-                    </li>
-                </ul>
-            </nav>
 
-            <Question/>
+            <Question />
 
         </div>
     </>
--- frontend/src/Question.js
+++ frontend/src/Question.js
@@ -15,24 +15,26 @@ const Question = () => {
 
 
 
-    return <>
-        <div className="question-input">
+    return <div className="container text-center">
+        <div className="input-group mb-4 mt-5">
             <input
                 type="text"
+                className="form-control"
                 placeholder="Enter your ethical question"
                 value={question}
-                onChange= {(e) => setQuestion(e.target.value)}
+                onChange={(e) => setQuestion(e.target.value)}
             />
         </div>
         <div className="options">
-            <button onClick={() => handleOptionClick('For')}>For</button>
-            <button onClick={() => handleOptionClick('Against')}>Against</button>
-            <button onClick={() => handleOptionClick('Neutral')}>Neutral</button>
+            <button className="btn btn-primary btn-block m-2" onClick={() => handleOptionClick('For')}>For</button>
+            <button className="btn btn-danger btn-block m-2" onClick={() => handleOptionClick('Against')}>Against</button>
+            <button className="btn btn-secondary btn-block m-2" onClick={() => handleOptionClick('Neutral')}>Neutral</button>
         </div>
-        <div className="result">
+        <div className="result mt-4">
             <p>{result}</p>
         </div>
-    </>
+    </div>
+
 }
 
 export default Question
